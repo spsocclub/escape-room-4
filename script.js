827b5c8d93c19915ebc4fb418cb42208d64ce1f2
ryan tan <tanryan777@gmail.com>
2019-10-31 21:04:43 +0800

    init

---
+++ script.js
@@ -0,0 +1,27 @@
+var CODE = 0811;
+
+$(document).ready(function() {
+  $(".myPinCode input").jqueryPincodeAutotab();
+});
+
+$(".myPinCode input").jqueryPincodeAutotab({
+  prevElement:null,
+  nextElement:null,
+  defaultFlow:true
+});
+
+
+function submitCode(){
+	var userCode = document.getElementById("code-input-1").value.toString() + document.getElementById("code-input-2").value.toString()  + document.getElementById("code-input-3").value.toString()  + document.getElementById("code-input-4").value.toString() ;
+
+	if(userCode == CODE){
+		document.getElementById("body").innerHTML = "<div id='container-3'><div class='main-text'>Congrats! You keyed in the correct code</div><button id='submit' onclick='goBack()''>GO BACK</button></div>";
+	} else {
+		document.getElementById("body").innerHTML = "<div id='container-2'><div class='main-text'>WRONG CODE</div><button id='submit' onclick='goBack()'>GO BACK</button></div>";
+	}
+}
+
+function goBack(){
+	document.getElementById("body").innerHTML = "<div id='container-1'><form onsubmit='submitCode()'><div class='main-text'>ENTER THE CODE</div><div class='myPinCode'><input type='tel' maxlength='1' class='pin' id='code-input-1'><input type='tel' maxlength='1' class='pin' id='code-input-2'><input type='tel' maxlength='1' class='pin' id='code-input-3'><input type='tel' maxlength='1' class='pin' id='code-input-4'></div><button id='submit' onclick='submitCode()' style='visibility: hidden;'></button></form></div>";
+	$(".myPinCode input").jqueryPincodeAutotab();
+}
